078f4b1497b1911b7e9c92bbf83ce2ead25f3831
John Sebastian Peterson <jpeterson57@gmail.com>
2025-01-02 06:25:01 +0000

    add ubuntu-repo

---
+++ gpkg/ubuntu-repo/build.sh
@@ -0,0 +1,8 @@
+set -x
+TERMUX_PKG_DESCRIPTION="Ubuntu repo in the same shell as bionic. operated by uapt and udpkg"
+# TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="DWTFYW"
+TERMUX_PKG_VERSION="1"
+TERMUX_PKG_DEPENDS="glibc"
+echo $TERMUX_BASE_DIR
+exit
