529836be6c45067bb0fd19d614b1ad75f69ac9c9
John Sebastian Peterson <jpeterson57@gmail.com>
2025-05-06 03:14:55 +0000

    forest confusion

--- gpkg/curl-impersonate/build.sh
+++ gpkg/curl-impersonate/build.sh
@@ -1,23 +1,21 @@
-# how do you build this fork
-# https://github.com/lexiforest/curl-impersonate
-
 # this insane script tries to build brotli and fail
-TERMUX_PKG_SRCURL=git+https://github.com/lwthiker/curl-impersonate
+TERMUX_PKG_SRCURL=git+https://github.com/lexiforest/curl-impersonate
+# TERMUX_PKG_SRCURL=git+https://github.com/lwthiker/curl-impersonate
 TERMUX_PKG_GIT_BRANCH=main
-TERMUX_PKG_BUILD_IN_SRC=true
+# TERMUX_PKG_BUILD_IN_SRC=true
 
 #to-do fix this proper build 
-# TERMUX_PKG_SRCURL=git+https://github.com/john-peterson/curl
-# TERMUX_PKG_GIT_BRANCH=imp
-# TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DUSE_NGHTTP2=ON -DCURL_BROTLI=ON -DOPENSSL_ROOT_DIR=$TERMUX_PREFIX/opt/boringssl -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX/opt/curl-impersonate  -DCMAKE_INSTALL_LIBDIR=$TERMUX_PREFIX/opt/curl-impersonate/lib "
+TERMUX_PKG_SRCURL=git+https://github.com/john-peterson/curl
+TERMUX_PKG_GIT_BRANCH=imp
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DUSE_NGHTTP2=ON -DCURL_BROTLI=ON -DCURL_USE_LIBPSL=OFF -DOPENSSL_ROOT_DIR=$TERMUX_PREFIX/opt/boringssl -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX/opt/curl-impersonate  -DCMAKE_INSTALL_LIBDIR=$TERMUX_PREFIX/opt/curl-impersonate/lib "
 # -DCMAKE_CXX_FLAGS:=-fno-exceptions
 
 TERMUX_PKG_DESCRIPTION="curl impersonation for curl_cffi"
 TERMUX_PKG_VERSION=8.1.1
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_BUILD_DEPENDS="golang"
-TERMUX_PKG_DEPENDS="boringssl-glibc, brotli-glibc, libnghttp2-glibc, zlib-glibc"
-# TERMUX_PKG_DEPENDS=" libnghttp2-glibc, libnghttp3-glibc, libssh2-glibc"
+TERMUX_PKG_DEPENDS="  brotli-glibc, libnghttp2-glibc, zlib-glibc, zstd-glibc"
+# TERMUX_PKG_DEPENDS=" boringssl-glibc, libnghttp2-glibc, libnghttp3-glibc, libssh2-glibc"
 
 # TERMUX_PKG_EXTRA_MAKE_ARGS=" -j4 "
 TERMUX_PKG_MAKE_PROCESSES=4
@@ -38,14 +36,15 @@ termux_step_post_configure() {
 : 
 }
 
-termux_step_make() { 
-export MAKEFLAGS=-j4
+# termux_step_make() { 
+# export MAKEFLAGS=-j4
 # make chrome-build -C ../build
 # sed -i '/unzip/i \\tpwd\n\tls' Makefile
 # ack -C pwd Makefile
 # exit
-make chrome-build 
-}
+# make chrome-build 
+# make build
+# }
 
 # termux_step_make_install() {
 	# mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib
